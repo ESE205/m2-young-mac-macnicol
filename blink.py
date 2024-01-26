e5119fd48a011d9ddf38e4b884c7a01a5cd41fee
David <d.m.young@wustl.edu>
2024-01-26 11:24:44 -0600

    Testing

--- blink.py
+++ blink.py
@@ -2,16 +2,23 @@ import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from time import sleep     # Import the sleep from time module
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
+import sys                 # Import Arvg
 
-ITER_COUNT = 15  
-pin1 = 11
+ITER_COUNT = 5  
+pin1 = 40                  # Pin for LED Output
+pin2 = 16                  # Pin for input
 
-GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)   
+GPIO.setup(pin2, GPIO.IN)
+GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)
+if len(sys.argv)>1:  
+   ITER_COUNT = int(sys.argv[1])    #If user enters a command line arg for iter.
 
-while ITER_COUNT > 0: # Run ITER_COUNT times
-   ITER_COUNT -= 1 # Decrement counter
-   GPIO.output(pin1, GPIO.HIGH) # Turn on
-   sleep(1)                     # Sleep for 1 second
-   GPIO.output(pin1, GPIO.LOW)  # Turn off
-   sleep(1)                     # Sleep for 1 second
-GPIO.cleanup()
+if GPIO.input(pin2):
+   while ITER_COUNT > 0: # Run ITER_COUNT times
+      ITER_COUNT -= 1 # Decrement counter
+      GPIO.output(pin1, GPIO.HIGH) # Turn on
+      sleep(1)                     # Sleep for 1 second
+      GPIO.output(pin1, GPIO.LOW)  # Turn off
+      sleep(1)                     # Sleep for 1 second
+   GPIO.cleanup()
+
