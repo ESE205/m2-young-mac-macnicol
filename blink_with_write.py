3476761abcb99880cb43c09e2c84181a362c0f57
Kenzie <k.b.macnicol@wustl.edu>
2024-01-26 12:06:44 -0600

    WooHoo

--- blink_with_write.py
+++ blink_with_write.py
@@ -20,20 +20,20 @@ ledPin = 40
 GPIO.setup(ledPin, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(inputPin, GPIO.IN)
 
-f = open("m2Prelab.txt","w")
+f = open("data.txt","w")
 
 for i in range(numLoops):
    if GPIO.input(inputPin):
       if GPIO.input(ledPin):
          GPIO.output(ledPin, GPIO.LOW)
-         f.write("\tSWITCH: ON\n\tLED: OFF\n\n")
+         f.write(f"{(i+1)*blinkPeriod}s\tSWITCH: ON\n\tLED: OFF\n\n")
       else:
          GPIO.output(ledPin, GPIO.HIGH)
-         f.write("\tSWITCH: ON\n\tLED: ON\n\n")
+         f.write(f"{(i+1)*blinkPeriod}s\tSWITCH: ON\n\tLED: ON\n\n")
    else:
       if GPIO.input(ledPin):
          GPIO.output(ledPin, GPIO.LOW)
-         f.write("\tSWITCH: OFF\n\tLED: OFF\n\n")
+         f.write(f"{(i+1)*blinkPeriod}s\tSWITCH: OFF\n\tLED: OFF\n\n")
 
    if DEBUG:
       if GPIO.input(inputPin):
